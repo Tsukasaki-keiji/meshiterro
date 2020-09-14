e55f1bcfdd7ba177c1a07fb6abd9c8bafce0848e
EC2 Default User <abcdquont1234@gmail.com>
2020-09-14 09:27:51 +0000

    Fix いいね、削除機能 form削除

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f9cd185afd90ac8c7abe01b52fa4ea5ef9ea1cc08fe7c2ca6fab3e2e7a6bd5ab1b86fae244699c3b52bc06f95222035bc16178a63130d8883491686f598c2775'
